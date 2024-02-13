1e410b8f41fc77d6c3b49047c4b0e1284899e12d
Emmanuel Okum <emmanuel.okum@idea2solutions.com>
2024-02-13 11:42:23 +0100

    Initial setUP

---
+++ packages/softec/servicegen/src/Servicegen.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Softec\Servicegen;
+
+use Illuminate\Support\ServiceProvider;
+
+class Servicegen extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
